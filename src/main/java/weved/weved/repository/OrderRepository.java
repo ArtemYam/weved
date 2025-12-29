765242c398904c2e64a24732682576860bd6cb08
weved <yad@weved.ru>
2025-12-29 15:31:51 +0300

    save zayavka 2

--- src/main/java/weved/weved/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package weved.weved.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import weved.weved.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
